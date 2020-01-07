3b3958120558ddefcbf8e71f2ef3b6032787fe3c
Shasheen8 <50681308+Shasheen8@users.noreply.github.com>
2020-01-07 16:13:33 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,16 +8,7 @@ import firebase from 'firebase'
 // Required for side-effects
 require("firebase/firestore");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyD7EJGtIV4jqph99PUIhJabRnAwtATfFYo",
-    authDomain: "first-chatbaby.firebaseapp.com",
-    databaseURL: "https://first-chatbaby.firebaseio.com",
-    projectId: "first-chatbaby",
-    storageBucket: "first-chatbaby.appspot.com",
-    messagingSenderId: "754766917107",
-    appId: "1:754766917107:web:263af2bb5b931b4104b686",
-    measurementId: "G-JMLC6WHER5"
-  };
+
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
